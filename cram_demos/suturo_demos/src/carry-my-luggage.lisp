cd21d0342c2144c663cb93e011a403790f974d38
Vanessa Hassouna <hassouna@uni-bremen.de>
2023-06-29 15:05:09 +0200

    [suturo-cml] stuff

--- cram_demos/suturo_demos/src/carry-my-luggage.lisp
+++ cram_demos/suturo_demos/src/carry-my-luggage.lisp
@@ -31,17 +31,19 @@
                          "geometry_msgs/PointStamped"
                          #'humanpose)
       (roslisp:subscribe "robokudovanessa/query/result"
+
                          "robokudo_msgs/QueryActionResult"
                          #'perceptresult)
 
       (if is-start
           (progn (talk-request "Hey, I am Toya i will help you to carry your Luggage." talk)
+                 (talk-request "When we arrived, please shake my hand!" talk)
+                 (talk-request "Is philipp finally here?." talk)
                                         ;(park-robot)
                                         ;(call-take-pose-action 0 0 0 0 0 -1.5 -1.5 0)
                  (talk-request "I am now searching for a human" talk))
           (talk-request "I lost you, please stop" talk))
-      
-      (block human-init
+
         ;;how to know human
         (loop until (not (or (eq (cpl::value human-detect-fluent-status) 4)
                              (eq (cpl::value human-detect-fluent-status) nil)))
@@ -50,14 +52,17 @@
                  (talk-request "Please step in front of me so I can recognize you" talk)
                  (sleep 1.5))
         
-        (when is-start
-          (talk-request "I was able to recognize you, Can you please give me the bag?" talk)
+       
+      (talk-request "I was able to recognize you" talk)
+      (when is-start
+        (talk-request "Can you please give me the bag?" talk)
                                         ;(call-take-pose-action 0 0 -0.65 0 -0.43 0 -1.17 -1.62)
                                         ;(human-assist talk))
-          )
         
+        )
         
-        (talk-request "I will now follow you, please dont move to fast." talk)
+      (talk-request "I will now follow you, please dont move to fast." talk)
+      (talk-request "When we arrived, please shake my hand!" talk)
         (let ((stop-condition t)
               
               (home-pose (cl-tf:make-pose-stamped "map" 0
@@ -78,109 +83,114 @@
             
             (cpl::seq
 
-              (su-real::with-hsr-process-modules 
                 (exe:perform (desig:a motion
-                                      (type cml))))
+                                      (type cml)
+                                      (laser-distance-threshold 0.5)))
 
               
               (setf stop-condition nil)))
           
           (unless stop-condition
             (cml-demo :is-start nil))
-          (talk-request "I would now drive back but philipp has not yet merged giskard stuff!" talk))))))
-          ;(call-nav-action-ps home-pose))))))
+          (talk-request "I would now drive back but philipp you have homework to do!" talk)
+          (exe:perform (desig:a motion
+                                (type cml)
+                                (drive-back t)
+                                (laser-distance-threshold 0.5)))))))
+                                        ;(call-nav-action-ps home-pose)))))
 
 
   
-(defun cml-demo (&key (step 0) (talk T) (is-start T))
-  (let ((percept-pub (roslisp::advertise "/robokudovanessa/query/goal"
-                                         "robokudo_msgs/QueryActionGoal"))
-        (human-detect-fluent-status
-          (cpl:make-fluent :name :human-detect-fluent-status))
-        (human-detect-fluent-position
-          (cpl:make-fluent :name :human-detect-fluent-position))
-        (hsr-pose-fluent
-          (cpl:make-fluent :name :hsr-pose-fluent)))
+;; (defun cml-demo (&key (step 0) (talk T) (is-start T))
+;;   (let ((percept-pub (roslisp::advertise "/robokudovanessa/query/goal"
+;;                                          "robokudo_msgs/QueryActionGoal"))
+;;         (human-detect-fluent-status
+;;           (cpl:make-fluent :name :human-detect-fluent-status))
+;;         (human-detect-fluent-position
+;;           (cpl:make-fluent :name :human-detect-fluent-position))
+;;         (hsr-pose-fluent
+;;           (cpl:make-fluent :name :hsr-pose-fluent)))
     
-    (flet ((perceptresult (msg)
-             (roslisp:with-fields
-                 ((?result (status status)))
-                 msg
-               (setf (cpl::value human-detect-fluent-status) ?result)))
+;;     (flet ((perceptresult (msg)
+;;              (roslisp:with-fields
+;;                  ((?result (status status)))
+;;                  msg
+;;                (setf (cpl::value human-detect-fluent-status) ?result)))
 
-           (humanpose (msg)
-             (setf (cpl::value human-detect-fluent-position) msg))
+;;            (humanpose (msg)
+;;              (setf (cpl::value human-detect-fluent-position) msg))
 
-           (hsrpose (msg)
-             (roslisp:with-fields ((?pose (pose)))
-                 msg
-               (setf (cpl::value hsr-pose-fluent) ?pose))))
+;;            (hsrpose (msg)
+;;              (roslisp:with-fields ((?pose (pose)))
+;;                  msg
+;;                (setf (cpl::value hsr-pose-fluent) ?pose))))
 
-      (roslisp:subscribe "/hsrb/base_pose"
-                         "geometry_msgs/PoseStamped"
-                         #'hsrpose)
-      (roslisp:subscribe "/robokudovanessa/human_position"
-                         "geometry_msgs/PointStamped"
-                         #'humanpose)
-      (roslisp:subscribe "robokudovanessa/query/result"
-                         "robokudo_msgs/QueryActionResult"
-                         #'perceptresult)
+;;       (roslisp:subscribe "/hsrb/base_pose"
+;;                          "geometry_msgs/PoseStamped"
+;;                          #'hsrpose)
+;;       (roslisp:subscribe "/robokudovanessa/human_position"
+;;                          "geometry_msgs/PointStamped"
+;;                          #'humanpose)
+;;       (roslisp:subscribe "robokudovanessa/query/result"
+;;                          "robokudo_msgs/QueryActionResult"
+;;                          #'perceptresult)
 
-      (if is-start
-          (progn (talk-request "Hey, I am Toya i will help you to carry your Luggage." talk)
-                 (park-robot)
-                 (talk-request "I am now searching for a human" talk))
-          (talk-request "I lost you, please stop" talk))
+;;       (if is-start
+;;           (progn (talk-request "Hey, I am Toya i will help you to carry your Luggage." talk)
+;;                  (park-robot)
+;;                  (talk-request "I am now searching for a human" talk))
+;;           (talk-request "I lost you, please stop" talk))
       
-      (block human-init
-        ;;how to know human
-        (loop until (not (or (eq (cpl::value human-detect-fluent-status) 4)
-                             (eq (cpl::value human-detect-fluent-status) nil)))
-              do
-                 (roslisp::publish-msg percept-pub )
-                 (talk-request "Please step in front of me so I can recognize you" talk)
-                 (sleep 1.5))
+;;       (block human-init
+;;         ;;how to know human
+;;         (loop until (not (or (eq (cpl::value human-detect-fluent-status) 4)
+;;                              (eq (cpl::value human-detect-fluent-status) nil)))
+;;               do
+;;                  (roslisp::publish-msg percept-pub )
+;;                  (talk-request "Please step in front of me so I can recognize you" talk)
+;;                  (sleep 1.5))
         
-        (when is-start
-          (talk-request "I was able to recognize you, Can you please give me the bag?" talk)
-                                        ;(human-assist talk))
-          (carry-robot))
+;;         (when is-start
+;;           (talk-request "I was able to recognize you, Can you please give me the bag?" talk)
+;;                                         ;(human-assist talk))
+;;           (carry-robot))
         
-        (talk-request "I will now follow you, please dont move to fast." talk)
-        (let ((stop-condition t)
+;;         (talk-request "I will now follow you, please dont move to fast." talk)
+;;          (talk-request "When we arrived Please shake my hand" talk)
+;;         (let ((stop-condition t)
               
-              (home-pose (cl-tf:make-pose-stamped "map" 0
-                                                  (cl-tf:make-3d-vector 0 0 0)
-                                                  (cl-tf:make-quaternion 0 0 0 1))))
-          (cpl::pursue
-            (cpl::seq
+;;               (home-pose (cl-tf:make-pose-stamped "map" 0
+;;                                                   (cl-tf:make-3d-vector 0 0 0)
+;;                                                   (cl-tf:make-quaternion 0 0 0 1))))
+;;           (cpl::pursue
+;;             (cpl::seq
               
 
-              (monitoring-mega-function)         
-              (talk-request "I think we arrived I hope my service was satisfactory!" talk)
-              (exe:perform (desig:a motion
-                                    (type gripper-motion)
-                                    (:open-close :open)
-                                    (effort 0.1)))
-              (setf stop-condition t))
-            (cpl::seq
-              (loop until (or (eq (cpl::value human-detect-fluent-status) 4)
-                              (eq (cpl::value human-detect-fluent-status) nil))
-                    do
-                       (let* ((nav-goal (relative-angle-to (cpl::value human-detect-fluent-position)
-                                                           (cpl::value hsr-pose-fluent))))
+;;               (monitoring-mega-function)         
+;;               (talk-request "I think we arrived I hope my service was satisfactory!" talk)
+;;               (exe:perform (desig:a motion
+;;                                     (type gripper-motion)
+;;                                     (:open-close :open)
+;;                                     (effort 0.1)))
+;;               (setf stop-condition t))
+;;             (cpl::seq
+;;               (loop until (or (eq (cpl::value human-detect-fluent-status) 4)
+;;                               (eq (cpl::value human-detect-fluent-status) nil))
+;;                     do
+;;                        (let* ((nav-goal (relative-angle-to (cpl::value human-detect-fluent-position)
+;;                                                            (cpl::value hsr-pose-fluent))))
 
-                         (cpl::pursue
-                           (progn (sleep 1)
-                                  (cpl::wait-for (cpl::pulsed human-detect-fluent-position)))
-                           (progn
-                             ;; (par 
-                             (call-nav-action-ps nav-goal)))))
-              (setf stop-condition nil)))
+;;                          (cpl::pursue
+;;                            (progn (sleep 1)
+;;                                   (cpl::wait-for (cpl::pulsed human-detect-fluent-position)))
+;;                            (progn
+;;                              ;; (par 
+;;                              (call-nav-action-ps nav-goal)))))
+;;               (setf stop-condition nil)))
           
-          (unless stop-condition
-            (cml-demo :is-start nil))
-          (call-nav-action-ps home-pose))))))
+;;           (unless stop-condition
+;;             (cml-demo :is-start nil))
+;;           (call-nav-action-ps home-pose))))))
 
         
 (defun monitoring-mega-function (&key (talk T))
@@ -194,7 +204,7 @@
           do
              
                (cpl:seq
-                 (talk-request "Please shake my hand" talk)
+                
                  
                  (setf hsr-monitoring-flex-up-bool nil)
                  (setf hsr-monitoring-flex-down-bool nil)
@@ -203,6 +213,7 @@
                   (desig:an action
                             (type monitoring-joint-state)
                             ;;todo change joint
+                            (comparison :lesser)
                             (joint-name "wrist_flex_joint")))
                  (let ((time-begin (cl::get-universal-time)))
                  (setf hsr-monitoring-flex-up-bool t)
